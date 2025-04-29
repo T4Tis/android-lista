ca82adf487dec0485c8fd8ba690532e53a5a14e6
T4Tis <147542190+T4Tis@users.noreply.github.com>
2025-04-28 22:34:22 -0300

    primeiro commit

---
+++ Android-lista-de-compras/app/src/main/java/t4tsi/github/android_lista_compra/model/ItemModel.kt
@@ -0,0 +1,12 @@
+package t4tsi.github.android_lista_compra.model
+
+import androidx.room.PrimaryKey
+
+data class ItemModel(
+
+
+    @PrimaryKey(autoGenerate = true)    // Anotação que indica que este campo é a chave primária da tabela.
+    val id: Int = 0,    // O valor padrão é 0, mas será substituído pelo valor gerado automaticamente pelo Room.
+
+    val name: String
+)
